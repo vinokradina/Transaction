b68f029e0b5a3a9ed5b057e2591a23d93414bfda
Alinka Vinokradinka <alinaivanova05@gmail.com>
2024-10-07 15:52:53 +0300

    1

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,110 @@
-package PACKAGE_NAME;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private String name;
+    private List<Transaction> transactions;
+    private static List<Account> accounts = new ArrayList<>();
+    private Date dateCreated;
+
+    public Account() {
+        this.transactions = new ArrayList<>();
+    }
+
+    public Account(int id, double balance, String name) {
+        this.id = id;
+        this.balance = balance;
+        this.name = name;
+        this.transactions = new ArrayList<>();
+        this.dateCreated = Date.from(Instant.now());
+        accounts.add(this);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public double getMonthlyInterest() {
+        return annualInterestRate / 100 / 12;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            transactions.add(new Transaction('W', amount, balance, "Withdrawal"));
+            System.out.println("Сумма снята со счета!");
+        } else {
+            System.out.println("Недостаточно средств на счёте.");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        transactions.add(new Transaction('D', amount, balance, "Deposit"));
+        System.out.println("Депозит успешно введен!");
+    }
+
+    public static Account findAccountById(List<Account> accounts, int id) throws IllegalArgumentException {
+        for (Account account : accounts) {
+            if (account.getId() == id) {
+                return account;
+            }
+        }
+        throw new IllegalArgumentException("Счета с таким id не найден!");
+    }
+
+    public static List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void displayAccountSummary() {
+        System.out.println("Account Summary for " + name);
+        System.out.println("Annual Interest Rate: " + annualInterestRate + "%");
+        System.out.println("Balance: " + balance + " руб.");
+        System.out.println("Transactions:");
+        for (Transaction transaction : transactions) {
+            System.out.println(transaction);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        Scanner in = new Scanner(System.in);
+//        Account first = new Account(1,12000);
+//        Account second = new Account(2,12000);
+//        Account third = new Account(3,10300);
+//        Account fourth = new Account(4,10000);
+//        Account fifth = new Account(5,10000);
+//        Account sixth = new Account(6,10000);
+//        Account seventh = new Account(7,10000);
+//        Account eighth = new Account(8,10000);
+//        Account ninth = new Account(9,10000);
+//        Account tenth = new Account(10,10000);
+//
+//        Account account = new Account(1155, 300000);
+//        account.setAnnualInterestRate(650);
+//        account.withdraw(16500);
+//        account.deposit(50000);
+//        System.out.println("===================================================");
+//        System.out.println("Номер счета : " + account.getId());
+//        System.out.println("Баланс : " + account.getBalance());
+//        System.out.println("Месячная ставка : " + account.getMonthlyInterest());
+//        System.out.println("Дата создания : " + account.getDateCreated());
+//        System.out.println("===================================================");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+//        while (true) {
+//
+//            while (true) {
+//                try {
+//                    System.out.println("Введите ID счета: ");
+//                    int id;
+//                    id = in.nextInt();
+//                    Account currentAccount = Account.findAccountById(Account.getAccounts(), id);
+//
+//                    // Show the main menu
+//                    boolean exitMenu = false;
+//                    while (!exitMenu) {
+//                        System.out.println("Основное меню\n1: Проверить баланс счета\n2: Снять со счета\n3: Положить на счет\n4: Выйти");
+//                        int choice = in.nextInt();
+//
+//                        switch (choice) {
+//                            case 1: // Проверка баланса
+//                                System.out.printf("Баланс счета %d: %.2f\n", id, currentAccount.getBalance());
+//                                break;
+//                            case 2: // Снятие со счета
+//                                System.out.println("Введите сумму для снятия:");
+//                                double amountWithdraw = in.nextDouble();
+//                                currentAccount.withdraw(amountWithdraw);
+//                                break;
+//                            case 3: // Пополнение счета
+//                                System.out.println("Введите сумму для пополнения:");
+//                                double amountDeposit = in.nextDouble();
+//                                currentAccount.deposit(amountDeposit);
+//                                break;
+//                            case 4: // Выход из меню
+//                                exitMenu = true;
+//                                break;
+//                            default:
+//                                System.out.println("Неверная команда! Пожалуйста, выберите 1, 2, 3 или 4.");
+//                        }
+//                    }
+//                } catch (IllegalArgumentException e) {
+//                    System.out.println(e.getMessage() + " Попробуйте еще раз.");
+//                } catch (java.util.InputMismatchException e) {
+//                    System.out.println("Введите корректный ID!");
+//                    in.next();
+//                }
+//            }
+//        }
+
+        Account account = new Account(1233, 1100, "Семен");
+        account.setAnnualInterestRate(7.5);
+
+        account.deposit(500);
+        account.deposit(600);
+        account.deposit(700);
+
+        account.withdraw(700);
+        account.withdraw(600);
+        account.withdraw(100);
+
+        account.displayAccountSummary();
     }
 }
+
